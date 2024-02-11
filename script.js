9b1402e834c15feefa5189697e44ac8f73280c67
bipul-eng <bipulkr2792003@gmail.com>
2024-02-11 14:00:14 +0530

    Added More Functionality

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ window.onload=reset;
 async function fetching()
 {   
     value2=value1.value;
+    if(value2 != "")
+    {
     const response=await fetch(apiLink+value2);
     var data=await response.json();
     document.getElementById("meaning").innerHTML=data[0].meanings[0].definitions[0].definition;
     song1=data[0].phonetics[0].audio;
+    }
+    else
+    {
+        document.getElementById("meaning").innerHTML="Please Enter A Character or a Word";
+    }
+    
 }
 function music(){
   new Audio(song1).play();
-}
-function myFunction()
-{
-    value1.innerHTML="";
 }
 function reset()
 {
